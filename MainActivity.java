10ee61e556127998b36c731b3903975d14b5a6f6
Khoi Ta <73479352+khoita2210@users.noreply.github.com>
2020-10-26 15:22:25 +0000

    Add files via upload
    Adding 9 button from 0 to 9 
    Lock the screen to vertical only 
    Adding background theme.
    Remove the Action Bar

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pawpatroladding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_activity);
+    }
+}
