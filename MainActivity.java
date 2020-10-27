af36c8f92bcc4be2ea24d178aa777202a7feade3
Khoi Ta <73479352+khoita2210@users.noreply.github.com>
2020-10-27 15:23:19 +0000

    Adding 9 logo and make the image draggable on the screen.

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,11 @@ package com.example.pawpatroladding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView; // for linking the ImageView object
+import android.view.MotionEvent; //for touch and drag event
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_activity);
+        ImageView paw,ryder,chase,sky,marshall,rocky,rubble,zuma,everest,rex;
+        paw= (ImageView) findViewById(R.id.logoPaw);
+        paw.setOnTouchListener(handleTouch);
+        ryder = (ImageView) findViewById(R.id.logoRyder);
+        ryder.setOnTouchListener(handleTouch);
+        chase = (ImageView) findViewById(R.id.logoChase);
+        chase.setOnTouchListener(handleTouch);
+        marshall = (ImageView) findViewById(R.id.logoMashall);
+        marshall.setOnTouchListener(handleTouch);
+        sky = (ImageView) findViewById(R.id.logoSky);
+        sky.setOnTouchListener(handleTouch);
+        rocky = (ImageView) findViewById(R.id.logoRocky);
+        rocky.setOnTouchListener(handleTouch);
+        rubble = (ImageView) findViewById(R.id.logoRubble);
+        rubble.setOnTouchListener(handleTouch);
+        zuma = (ImageView) findViewById(R.id.logoZuma);
+        zuma.setOnTouchListener(handleTouch);
+        everest = (ImageView) findViewById(R.id.logoEverest);
+        everest.setOnTouchListener(handleTouch);
+        rex = (ImageView) findViewById(R.id.logoEverest);
+        rex.setOnTouchListener(handleTouch);
+
+
+
+
+
+
     }
+
+
+    private View.OnTouchListener handleTouch = new View.OnTouchListener() {
+        float dX, dY;
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    dX = view.getX() - event.getRawX();
+                    dY = view.getY() - event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    view.animate()
+                            .x(event.getRawX() + dX)
+                            .y(event.getRawY() + dY)
+                            .setDuration(0)
+                            .start();
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+    };
+
+
 }
